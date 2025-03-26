b1f4ee5d12f4bdce23a0763a1bfd27122254e980
AjoJosee <ajojoseee@gmail.com>
2025-03-27 01:44:23 +0530

    Set up webpack configuration and update package.json for proper project bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
-import path from 'path';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-export default {
-  entry: './index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve('dist'),
-  },
-  mode: 'development',
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'styles.css', to: 'styles.css' },
+                { from: 'maps.js', to: 'maps.js' },
+                { from: 'events.html', to: 'events.html' },
+                { from: 'leaderboard.html', to: 'leaderboard.html' },
+                { from: 'community.html', to: 'community.html' },
+                { from: 'resources.html', to: 'resources.html' }
+            ]
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        proxy: {
+            '/api': 'http://localhost:3000'
+        }
+    }
 };
